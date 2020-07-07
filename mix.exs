e4062d06efd20c3329b5383e2698b43cb7c53e8c
Alek Ratzloff <alekratz@gmail.com>
2020-07-06 17:02:21 -0700

    Update mix.exs to require only version 1.7 (which is what Debian 10 uses)
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Omnibot.MixProject do
     [
       app: :omnibot,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
