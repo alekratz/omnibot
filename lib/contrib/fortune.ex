f89b31520bc0d37963bda085195e12ef8c47db32
Alek Ratzloff <alekratz@gmail.com>
2020-08-11 12:45:29 -0700

    Add some more fortunes for fortune module
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- lib/contrib/fortune.ex
+++ lib/contrib/fortune.ex
@@ -8,13 +8,37 @@ defmodule Omnibot.Contrib.Fortune do
     "Average Luck",
     "Bad Luck",
     "Good news will come to you by mail",
-    "´_ゝ`",
-    "ﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!",
     "You will meet a dark handsome stranger",
     "Better not tell you now",
     "Outlook good",
     "Very Bad Luck",
     "Godly Luck",
+
+    "Outlook bad",
+    "Today will be a good day!",
+    "Pack a raincoat",
+    "Fair Luck",
+    "Good weather for cocks",
+    "Look towards the setting sun",
+    #"It's been years",
+    #"God I wish that were me",
+    "bad!",
+    "Love is in your future",
+    "You were young and foolish then, you are old and foolish now.",
+    "Delete your account",
+    "Rest in piss",
+    "What a horrible night to have a curse.",
+    "Closed: WONTFIX",
+    "You've got mail!",
+    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    "AAAAAAAAAAAAAAAAAA",
+
+    "´_ゝ`",
+    "ﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!",
+
+    "¯\_(ツ)_/¯",
+    ":^)",
+
   ]
 
   command "!fortune", [to] do
