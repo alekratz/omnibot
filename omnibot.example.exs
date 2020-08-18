09cb4ac752d5eebbcce368ab57536f5662e1fd34
Alek Ratzloff <alekratz@gmail.com>
2020-08-18 16:33:43 -0700

    Update config with some docs
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- omnibot.example.exs
+++ omnibot.example.exs
@@ -1,18 +1,23 @@
 alias Omnibot.Config
 
 config = %Config {
-  nick: "omnibot_testing",
-  server: "irc.bonerjamz.us",
+  nick: "omnibot",
+  server: "chat.freenode.net",
   port: 6667,
   ssl: false,
 
   plugins: [
+    # Required for operation of Omnibot.
+    # Core can be replaced with another implementation, but generally this should be in every config.
     Omnibot.Core,
+    # Use OnConnect module to register commands
     {Omnibot.Contrib.OnConnect, commands: [
       ["privmsg", "nickserv", "register", "password123", "omnibot@omni.bot"],
       ["privmsg", "nickserv", "identify", "password123"]
     ]},
+    # Use Linkbot to get titles of URLs posted in the chat
     {Omnibot.Contrib.Linkbot, channels: :all},
+    # Fortune will spit out fun messages with the !fortune command - try it out!
     {Omnibot.Contrib.Fortune, channels: :all},
     {Omnibot.Contrib.Wordbot, channels: ["#idleville"], ignore: ["username"]},
   ],
